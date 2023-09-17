3a26bdd40044db0715f89e72667d2db9cd25aa5b
CookieMusicStar <daphanie168@gmail.com>
2023-09-17 07:54:17 -0500

    Completed Ruin

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,40 +1,44 @@
 package studio2;
-
 import java.util.Scanner;
-
 public class Ruin {
-
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("How much are you starting with?");
+		System.out.print("How much are you starting with? ");
 		int startAmount = in.nextInt();
-		System.out.print("How likely are you to win today?");
+		System.out.print("How likely are you to win today? ");
 		double winChance = in.nextDouble();
-		System.out.print("At how much money do you want to stop gambling?");
+		System.out.print("At how much money do you want to stop gambling? ");
 		int winLimit = in.nextInt();
-		while (startAmount < winLimit && startAmount > 0)
-		{
-			double number = Math.random();
-			if (number > (1 - winChance))
+		System.out.print("How many days do you want to play? ");
+		int totalSimulations = in.nextInt();
+		
+		for (int count = 1; count <= totalSimulations; count++) {
+			
+			int newStartAmount = startAmount;
+			for (int play = 1; newStartAmount < winLimit && newStartAmount > 0; play++)
 			{
-				System.out.println("Win!");
-				startAmount++;
+				double number = Math.random();
+				
+				if (number > (1 - winChance))
+				{
+					// System.out.print("Win!");
+					newStartAmount++;
+				}
+				
+				else
+				{
+					// System.out.println("Loss!");
+					newStartAmount--;
+				}
+				if (newStartAmount == winLimit){
+					System.out.println("Simulation: " + count + " " + play + " Success");
+				}
+				
+				else if (newStartAmount == 0)
+				{
+					System.out.println("Simulation: " + count + " " + play + " Ruin");
+				}
 			}
-			else
-			{
-				System.out.println("Loss!");
-				startAmount--;
-			}
 		}
-		if (startAmount == winLimit)
-		{
-			System.out.println("Success!");
-		}
-		else if (startAmount == 0)
-		{
-			System.out.println("Ruin!");
-		}
-
 	}
-
 }
